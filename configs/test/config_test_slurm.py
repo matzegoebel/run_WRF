1aa42314035251108b93883cc87f286b3678a1f6
Matthias Göbel <matthias.goebel@uibk.ac.at>
2020-01-08 17:13:43 +0100

    qos change

--- configs/test/config_test_slurm.py
+++ configs/test/config_test_slurm.py
@@ -21,7 +21,7 @@ module_load = "module load intel/16.0.3 intel-mpi/5.1.3 hdf5/1.8.16 pnetcdf/1.5.
                export NETCDF=/opt/sw/x86_64/glibc-2.12/ivybridge-ep/netcdf/4.3.2/intel-14.0.2;\
                export PNETCDF=/opt/sw/x86_64/glibc-2.12/ivybridge-ep/parallel/netcdf/1.5.0/intel-14.0.2"
 queue = "mem_0064" #partition on vsc3
-qos = "idle_0064"
+qos = "normal_0064"
  #minimum pool size; should be equal to the number of available CPUs per node
 pool_size = 8
 request_vmem = False
