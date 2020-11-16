b78041d5b641239438c2ca83aab621333f4bc55f
Matthias Göbel <matthias.goebel@uibk.ac.at>
2020-11-16 09:59:40 +0100

    deleted deprecated code

--- run_wrf/misc_tools.py
+++ run_wrf/misc_tools.py
@@ -545,11 +545,8 @@ def get_vmem(runs):
         return
     vmem = []
     for i, r in enumerate(runs):
-        #rfiles = glob.glob(r + "/vmemusage_VIRT*")
         rfiles = glob.glob(r + "/resources*")
         for resource_file in rfiles:
-            # vmem_r = np.loadtxt(resource_file)
-            # if len(vmem_r) > 0:
             vmem_r_str = get_job_usage(resource_file)["maxvmem"]
             vmem_r = None
             for mag, factor in zip(("M", "G"), (1, 1000)):
@@ -557,7 +554,6 @@ def get_vmem(runs):
                     vmem_r = float(vmem_r_str[:vmem_r_str.index(mag)])*factor
                     break
             vmem.append(vmem_r)
-            #vmem.append(max(vmem_r)/1024)
     if len(vmem) > 0:
         return vmem
 
